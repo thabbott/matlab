16d77ce375309e6685b01a74f5b38223027a635d
Tristan Abbott <tabbott0312@gmail.com>
2018-03-28 14:37:36 -0400

    Initial commit

---
+++ colormaps/rdbu.m
@@ -0,0 +1,90 @@
+function newmap = rdbu(m)
+%RDBU    Red-White-Blue Color Map
+%   RDBU(M) returns an M-by-3 matrix containing a red-blue diverging colormap
+%   RDBU, by itself, is the same length as the current figure's
+%   colormap. If no figure exists, MATLAB creates one.
+%
+%   Example:
+%       imagesc(peaks(500))
+%       colormap(rdbu); colorbar
+%
+%   Example:
+%       load topo
+%       imagesc(0:360,-90:90,topo), axis xy
+%       colormap(rdbu); colorbar
+
+if nargin < 1
+    m = size(get(gcf,'colormap'),1);
+end
+
+bottom = [0 0 0.5];
+botmiddle = [0 0.5 1];
+middle = [1 1 1];
+topmiddle = [1 0 0];
+top = [0.5 0 0];
+
+% Find middle
+lims = get(gca, 'CLim');
+
+% Find ratio of negative to positive
+if (lims(1) < 0) & (lims(2) > 0)
+    % It has both negative and positive
+    % Find ratio of negative to positive
+    ratio = abs(lims(1)) / (abs(lims(1)) + lims(2));
+    neglen = round(m*ratio);
+    poslen = m - neglen;
+    
+    % Just negative
+    new = [bottom; botmiddle; middle];
+    len = length(new);
+    oldsteps = linspace(0, 1, len);
+    newsteps = linspace(0, 1, neglen);
+    newmap1 = zeros(neglen, 3);
+    
+    for i=1:3
+        % Interpolate over RGB spaces of colormap
+        newmap1(:,i) = min(max(interp1(oldsteps, new(:,i), newsteps)', 0), 1);
+    end
+    
+    % Just positive
+    new = [middle; topmiddle; top];
+    len = length(new);
+    oldsteps = linspace(0, 1, len);
+    newsteps = linspace(0, 1, poslen);
+    newmap = zeros(poslen, 3);
+    
+    for i=1:3
+        % Interpolate over RGB spaces of colormap
+        newmap(:,i) = min(max(interp1(oldsteps, new(:,i), newsteps)', 0), 1);
+    end
+    
+    % And put 'em together
+    newmap = [newmap1; newmap];
+    
+elseif lims(1) >= 0
+    % Just positive
+    new = [middle; topmiddle; top];
+    len = length(new);
+    oldsteps = linspace(0, 1, len);
+    newsteps = linspace(0, 1, m);
+    newmap = zeros(m, 3);
+    
+    for i=1:3
+        % Interpolate over RGB spaces of colormap
+        newmap(:,i) = min(max(interp1(oldsteps, new(:,i), newsteps)', 0), 1);
+    end
+    
+else
+    % Just negative
+    new = [bottom; botmiddle; middle];
+    len = length(new);
+    oldsteps = linspace(0, 1, len);
+    newsteps = linspace(0, 1, m);
+    newmap = zeros(m, 3);
+    
+    for i=1:3
+        % Interpolate over RGB spaces of colormap
+        newmap(:,i) = min(max(interp1(oldsteps, new(:,i), newsteps)', 0), 1);
+    end
+    
+end
---
+++ standard_figure.m
@@ -0,0 +1,14 @@
+%% Generate figures with standard screen-independent sizes
+function fig = standard_figure(ftype)
+
+    fig = figure('Units', 'Inches', 'PaperUnits', 'Inches');
+    switch ftype
+        case 'medium'
+            set(fig, 'Position', [1 1 8 6], 'PaperSize', [8 6]);
+        case 'wide'
+            set(fig, 'Position', [1 1 12 6], 'PaperSize', [12 6]);
+        otherwise
+            error('Unrecognized standard figure: %s', ftype);
+    end
+
+end
